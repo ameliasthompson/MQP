cf820651a58edd3e3dd01c399fd29dac4b4b9949
Amelia Thompson <ameliasthompson32@gmail.com>
2021-03-30 22:51:43 -0400

    Added listener to enable and disable button based on what's in the textbox and checkbox

--- docs/main.js
+++ docs/main.js
@@ -4,11 +4,31 @@ function main() {
     setupDisclaimer();
 }
 
+function consentListener() {
+    var agefieldval = document.getElementById('agefield').value;
+
+    // Bounds check and check for consent
+    if (agefieldval != null && agefieldval > 0
+        && document.getElementById('consentbox').checked) {
+            document.getElementById('continuebutton').disabled = false;
+        
+    } else {
+        document.getElementById('continuebutton').disabled = true;
+    }
+}
+
 function setupDisclaimer() {
+    var agefield = document.getElementById('agefield');
+    var consentbox = document.getElementById('consentbox');
+
     // Make sure these are all set correctly.
-    document.getElementById('agefield').value = null;
-    document.getElementById('consentbox').checked = false;
+    agefield.value = null;
+    consentbox.checked = false;
     document.getElementById('continuebutton').disabled = true;
+
+    // Register listeners.
+    agefield.addEventListener('keyup', consentListener);
+    consentbox.addEventListener('change', consentListener);
 
     document.getElementById('disclaimer').hidden = false;
 }
